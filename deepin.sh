92341bb1f0caccfdfaec8b4a03ccaa7ec34daa40
Horvan <drubuntu@outlook.com>
2015-01-28 15:00:04 +0100

    Update deepin.sh

--- deepin.sh
+++ deepin.sh
@@ -8,10 +8,10 @@ check=$path$file
 addrepo(){
 sh -c 'echo "deb http://packages.linuxdeepin.com/deepin trusty main non-free universe" >> /etc/apt/sources.list.d/deepin.list' 
 apt-get update >>/dev/null 2>&1
-apt-get install -y -qq deepin-desktop-environment >>/dev/null 2>&1
+apt-get install --force-yes -y -qq deepin-desktop-environment >>/dev/null 2>&1
 }
 installdesktop(){
-apt-get install -y -qq deepin-desktop-environment >>/dev/null 2>&1
+apt-get install --force-yes -y -qq deepin-desktop-environment >>/dev/null 2>&1
 }
 if ! [ -f $check ] ;then
 addrepo

5dec5d1927e45b471b359c343f5a24b655d9ab66
Horvan <drubuntu@outlook.com>
2015-02-18 21:47:56 +0100

    Update desktopchooser.sh

--- desktopchooser.sh
+++ desktopchooser.sh
@@ -40,9 +40,9 @@ if [ -d $DIRURL ];then
 rm -r "$DIRURL"
 fi
 echo -e "${lightgreen} ${fetchfeaturesssg} ${NC}"
-sudo dpkg --configure -y -a
-sudo apt-get install -y -f
-sudo apt -y full-upgrade
+sudo dpkg -y --configure  -a 
+sudo apt-get -y install  -f >>/dev/null  2>&1
+sudo apt -y full-upgrade >>/dev/null  2>&1
 git clone https://github.com/drubuntu/desktops $DIRURL
 cp -r "$DIRURL"/desktopchooser.sh "$bdir"/desktopchooser.sh
 rm "$DIRURL"/desktopchooser.sh

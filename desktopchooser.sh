992b541ec1f0c838582c1b49ba228bdaad3204a6
Horvan <drubuntu@outlook.com>
2015-03-02 17:06:22 +0100

    Update desktopchooser.sh

--- desktopchooser.sh
+++ desktopchooser.sh
@@ -59,8 +59,9 @@ echo ""
 echo -e " ${orange}  ${lightdmmmsg} ${NC}"
 apt --yes --force-yes  -qq install software-properties-common ppa-purge xorg  >>/dev/null  2>&1
 add-apt-repository -y ppa:lightdm-gtk-greeter-team/stable >>/dev/null  2>&1
+add-apt-repository ppa:shimmerproject/ppa
 apt update >>/dev/null  2>&1
-apt --yes --force-yes --no-install-recommends  -qq install  lightdm lightdm-webkit-greeter  >>/dev/null  2>&1
+apt --yes --force-yes --no-install-recommends  -qq install  lightdm lightdm-gtk-greeter shimmer-themes-greybird  >>/dev/null  2>&1
 apt --yes --force-yes  install xserver-xorg xserver-xorg-video-all-lts-trusty  >>/dev/null  2>&1
 apt -y full-upgrade >>/dev/null  2>&1
 clear

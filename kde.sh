4bac4500c5f1f129192c966d699056fa0457d37f
Horvan <drubuntu@outlook.com>
2015-03-02 18:04:41 +0100

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -3,10 +3,14 @@ restplymouth(){
 #reset plymouthlogo if dist-upgrade change logo
 sudo rm /lib/plymouth/themes/default. plymouth
 sudo ln -s /lib/plymoouth/themes/drubuntu/drubuntu.plymouth /lib/plymouth/default.plymouth
+apt-get -qq remove lightdm-kde-greeter
+	
 }
 installdesktop(){
 apt --no-install-recommends -y -qq  install kubuntu-desktop 
 apt -y dist-upgrade 	
+ 
+	
 }
 echo "Installing KDE ..."
 echo ""
